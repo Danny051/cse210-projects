d89383e68864f18485ca95ecca25f5bc300cffe4
Danny051 <danzzy051@gmail.com>
2023-09-14 21:01:30 +0100

    Added a new file

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,13 +4,51 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("What is your first name?");
-        string first_name = Console.ReadLine();
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
         
-        Console.Write("What is your last name?");
-        string last_name = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
         
         Console.WriteLine("");
-        Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}.");
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
+
+        Console.Write("What is your grade? ");
+        string valueFromUser = Console.ReadLine();
+
+        int x = int.Parse(valueFromUser);
+        
+        string letter = "";
+        if (x >= 90)
+        {
+            letter = "A";
+        }
+        else if (x >= 80)
+        {
+            letter = "B";
+        }
+        else if (x >= 70)
+        {
+            letter = "C";
+        }
+        else if (x >= 60)
+        {
+            letter = "D";
+        }
+        else 
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is {letter}");
+        
+        if (x >= 70)
+            {
+                Console.WriteLine("You passed!");
+            }
+        else
+            {
+                Console.WriteLine("Better luck next time!");
+            }
     }
 }
