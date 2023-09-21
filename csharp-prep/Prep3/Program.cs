f3f6a5c24fbd0dd66f1b932110f4cc1bccd8b3ee
Danny051 <danzzy051@gmail.com>
2023-09-21 19:57:56 +0100

    added new files

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,54 @@
 using System;
+using System.Threading.Tasks.Dataflow;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Console.WriteLine("Hello Prep3 World!");
+        // Console.Write("What is the magic number? ");
+        // string magicNumber = Console.ReadLine();
+        // int mNumber = int.Parse(magicNumber);
+        
+        Random randomGenerator = new Random();
+        int mNumber = randomGenerator.Next(1, 11);
+        string keepPlaying = "yes";
+        while (keepPlaying == "yes")
+        {
+            // Console.Write("What is your guess? ");
+            // string guessNumber = Console.ReadLine();
+
+            int gNumber = 0;
+            int guess_count = 0;
+
+            while (mNumber != gNumber)
+            {
+                Console.Write("What is your guess? ");
+                gNumber = int.Parse(Console.ReadLine());
+                guess_count++;
+
+                if (mNumber > gNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (mNumber < gNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed right!");
+                }
+            }
+            Console.WriteLine($"It took you {guess_count} guesses");
+            
+            
+            Console.Write("Would you like to play again? ");
+            keepPlaying = Console.ReadLine();
+        }
+        // else
+        // {
+        //     Console.Write("Thank you for playing. Goodbye.");
+        // }
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        // Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        // Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        int userNumber = -1;
+        while (userNumber != 0)
+        {
+            Console.Write("Enter number: ");
+            string userResponse = Console.ReadLine();
+            userNumber = int.Parse(userResponse);
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+        }
+
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        Console.WriteLine(sum);
+        
     }
 }
