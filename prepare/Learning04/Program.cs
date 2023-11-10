25c19aa2f67fcef7d3f6a2e288c48b5bfbf5b744
Danny051 <danzzy051@gmail.com>
2023-11-10 23:02:17 +0100

    initial commit

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        // Console.WriteLine("Hello Learning04 World!");
+
+        MathAssignment assignment1 = new MathAssignment("Roberto Rodriguez", "Fractions", "Section 7.3", "Problems 8-19");
+        Console.WriteLine("Math Assignment");
+        Console.WriteLine(assignment1.GetHomeworkList());
+
+        WritingAssignment assignment2 = new WritingAssignment("Mary Waters","European History","The Causes of World War II by Mary Waters");
+        Console.WriteLine("\nWriting Assignment");
+        Console.WriteLine(assignment2.GetWritingInformation());
+
     }
 }
